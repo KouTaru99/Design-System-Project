31d142cf9e5a5922099e2e7817176b5db56196e7
KouTaru99 <nguyentansang030399@gmail.com>
2024-04-08 22:59:20 +0700

    #1 - repush request for issue 1

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -5,29 +5,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatMenu, MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
+
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatMenuModule, MatIconModule],
+  imports: [MatSidenavModule, MatFormFieldModule, MatSelectModule, MatButtonModule, MatMenuModule, MatIconModule, ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
 export class SidenavComponent {
-  timedOutCloser:any
-  @ViewChild(MatMenu) animals!: QueryList<MatMenu>;
 
-  mouseEnter(trigger:any) {
-    console.log(this.animals);
-    // if (this.timedOutCloser) {
-    //   clearTimeout(this.timedOutCloser);
-    // }
-    // trigger.openMenu();
-  }
-
-  mouseLeave(trigger:any) {
-  //   this.timedOutCloser = setTimeout(() => {
-  //     trigger.closeMenu();
-  //   }, 50);
-  }
 
 }
